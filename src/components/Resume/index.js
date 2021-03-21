7add20f5e825c9fa76e853392867ff1c6eaf985f
dcharney <delaneycharney@gmail.com>
2021-03-21 13:39:30 +0100

    resume displayed in iframe and downloadable

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import resumePdf from '../../assets/Resume/delaneyCharney.pdf';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 
 function Resume() {
     return (
-        <section>
-            Resume
-            <a href={resumePdf} download>
+        <section className="resume">
+            {/* <a href={resumePdf} download>
                 <FontAwesomeIcon icon={ faDownload } size="lg" className="d-inline-block" />
-            </a> 
+            </a>  */}
+            {/* <Document file={resumePdf} >
+                <Page pageNumber={1} />
+            </Document> */}
+            <iframe src={resumePdf} allowfullscreen="" frameborder="0" height="100%" width="80%" title="Displaying My Resume" scrolling="auto"></iframe>
         </section>
     );
 }
