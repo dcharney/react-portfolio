372c9ff91df77404b6cc017ce847ddfc6d7249fa
dcharney <delaneycharney@gmail.com>
2021-03-21 10:54:34 +0100

    selected nav component stays highlighted

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBug } from '@fortawesome/free-solid-svg-icons';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,11 +6,14 @@ import Nav from 'react-bootstrap/Nav';
 
 
 function Header() {
-    const categories = [
+    
+    const [categories] = useState([
         { name: "About Me", description: "Everything you could want to know about me!" },
         { name: "Portfolio", description: "Some samples of my work." },
-        { name: "Resume", description: "You can download my resume here." }
-    ];
+        { name: "Resume", description: "You can download my resume here." },
+        { name: "Contact", description: "Want to collaborate? Want to hire me?" }
+    ]);
+    const [ currentCategory, setCurrentCategory ] = useState(categories[0]);
 
     function categorySelected(name) {
         console.log(`${name}`);
@@ -26,9 +29,9 @@ function Header() {
                     Delaney Charney</Navbar.Brand>
                 <Nav className="mr-auto">
                     { categories.map((category) => (
-                        <Nav.Link key={category.name}><span onClick={() => categorySelected(category.name)} >{category.name}</span></Nav.Link>
+                        <Nav.Link className={`${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+                            <span onClick={() => { setCurrentCategory(category) }} >{category.name}</span></Nav.Link>
                     ))}
-                    <Nav.Link href="#contact">Contact</Nav.Link>
                 </Nav>
             </Navbar>
         </header>
